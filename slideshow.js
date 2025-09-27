2346eda5f27550c6affb58267471f1adb4233112
By_DoethsZ <125604058+DoethsZ-exekute@users.noreply.github.com>
2025-09-27 09:30:10 -0400

    Update slideshow.js

--- slideshow.js
+++ slideshow.js
@@ -1,1 +1,19 @@
+const images = [
+  "img/tux1.png",
+  "img/tux2.png",
+  "img/tux3.png",
+  "img/tux4.png"
+  // Ajoute ici tous les chemins vers tes images
+];
+
+let index = 0;
+const imgElement = document.getElementById("slideshow");
+
+function showNextImage() {
+  imgElement.src = images[index];
+  index = (index + 1) % images.length;
+}
+
+setInterval(showNextImage, 3000); // Change toutes les 3 secondes
+showNextImage(); // Affiche la première image au chargement
 
